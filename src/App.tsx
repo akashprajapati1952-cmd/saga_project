a3a6be629a76e31c51c37a3abb8c93bffdee9d8c
akashprajapati1952-cmd <akashprajapati1952@gmail.com>
2026-05-10 20:25:58 +0530

    github pages deployment success

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import ShowDetailPage from "./Pages/ShowDetails.Page";
 import ShowListPage from "./Pages/ShowsList.Page";
 
 function App() {
   return (
     <div className="max-w-5xl mx-auto">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<ShowListPage />} />
           <Route path="show/:showId" element={<ShowDetailPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
